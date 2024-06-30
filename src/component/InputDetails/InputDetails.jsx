1bd6910d3c40d835af13798be179551a0009e020
shirishSIB <shirish.bajpai@sendinblue.com>
2024-06-30 21:11:26 +0530

    removed bootstrap

--- src/component/InputDetails/InputDetails.jsx
+++ src/component/InputDetails/InputDetails.jsx
@@ -1,11 +1,18 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import Translations from '../Common/Translations';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { putCandidateDetails, updateCandidate } from '../../actions/candidate';
 import './InputDetails.css';
 import { getCurrentDate } from '../Common/helper';
+import Modal from "https://cdn.skypack.dev/react-modal@3.16.1";
+
+const customStyles = {
+    content: {
+        margin: '5%',
+        border: '0.5px ridge black',
+        boxShadow: '0.5rem 0.5rem 2rem 0.5rem rgb(161, 161, 161)',
+    },
+};
 
 const InputDetails = ({ handleClose, show, candidate }) => {
     const dispatch = useDispatch();
@@ -33,38 +40,38 @@ const InputDetails = ({ handleClose, show, candidate }) => {
         handleClose()
     }
 
+
     return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {Object.keys(basicInfo).map((detail) =>
-                    !['last_update'].includes(detail) && <div className="mb-3" key={detail}>
-                        <label for="exampleFormControlInput1" className="form-label">
-                            <Translations id={detail} />
-                        </label>
-                        <input type="email" className="form-control" value={basicInfo[detail]} onChange={(e) => handleBasicInfo(e, detail)} />
-                    </div>
-                )}
-                {Object.keys(jobDetails).map((detail) =>
-                    <div className="mb-3" key={detail}>
-                        <label for="exampleFormControlInput1" className="form-label">
-                            <Translations id={detail} />
-                        </label>
-                        <input type="email" className="form-control" value={jobDetails[detail]} onChange={(e) => handleJobDetails(e, detail)} />
-                    </div>
-                )}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleSave}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-        </Modal>
+
+        <Modal
+            isOpen={show}
+            onRequestClose={handleClose}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+            {Object.keys(basicInfo).map((detail) =>
+                !['last_update'].includes(detail) && <div className="form-field" key={detail}>
+                    <label className="form-label">
+                        <Translations id={detail} />
+                    </label><br />
+                    <input type="email" className="form-input" value={basicInfo[detail]} onChange={(e) => handleBasicInfo(e, detail)} />
+                </div>
+            )}
+            {Object.keys(jobDetails).map((detail) =>
+                <div className="form-field" key={detail}>
+                    <label className="form-label">
+                        <Translations id={detail} />
+                    </label><br />
+                    <input type="email" className="form-input" value={jobDetails[detail]} onChange={(e) => handleJobDetails(e, detail)} />
+                </div>
+            )}
+            <div className='button'>
+                <button onClick={handleClose} className="close-button">Close</button>
+                <button onClick={handleSave} className="save-button">Save Changes</button>
+            </div>
+        </Modal >
+
+
     );
 }
 
